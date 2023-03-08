6319216e52d5776b48ec6fbebfc4efcf99ae0bc7
fivetran-reneeli <renee.li@fivetran.com>
2023-03-08 12:33:04 -0500

    clean

--- models/netsuite/tmp/stg_netsuite__vendors_tmp.sql
+++
@@ -1,4 +0,0 @@
-{{ config(enabled=var('netsuite_data_model', 'netsuite') == 'netsuite') }}
-
-select * 
-from {{ var('netsuite_vendors') }}
--- models/netsuite2/tmp/stg_netsuite2__locations_tmp.sql
+++
@@ -1,4 +0,0 @@
-{{ config(enabled=var('netsuite_data_model', 'netsuite') == var('netsuite2_variable_name','netsuite2')) }}
-
-select * 
-from {{ var('netsuite2_locations') }}
